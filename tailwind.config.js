4e5d854ec8e4781cbf7c702bf0c47503d484a031
ibrohimovabduraxim75-debug <ibrohimovabduraxim75@gmail.com>
2026-08-30 06:08:03 +0500

    Header is done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["/*.{html,js}"],
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'pop': ["Poppins"],
+      },
+    },
   },
   plugins: [],
 }
